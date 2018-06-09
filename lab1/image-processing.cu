f66394bfc39e3e230d54c5d35dfdcbdba3973a12
sspatari <spatari.stanislav@gmail.com>
2018-06-09 23:21:05 +0200

    Solved lab1 with image blurring

--- lab1/image-processing.cu
+++ lab1/image-processing.cu
@@ -6,18 +6,36 @@
 
 #include "lodepng.h"
 
+__device__ int clamp(int value, int lo, int hi) {
+    return (value < lo ? lo : value > hi ? hi : value);
+}
+
 __global__ void img_proc(const unsigned char* IMG_IN, unsigned char* IMG_OUT, int W, int H)
 {
-    /* TODO: write a kernel that is executed for each pixel (x,y) in the image
+    /* Kernel that is executed for each pixel (x,y) in the image
      * and does the following:
      * compute the average value of the 5x5 neighborhood of (x,y):
      * sum up all IMG_IN pixels with horizontal indices x-2, ..., x+2 and
      * vertical indices y-2, ..., y+2, then divide by 25 and write
      * the result to IMG_OUT.
-     * Make sure all indices are within valid range (at borders, you can simply
-     * clip your neighborhood: if x is less than 0 then set it to 0, and similar
+     * All indices are within valid range (at borders,
+     * neighborhoods are clipped: if x is less than 0 then set it to 0, and similar
      * for the other borders).
      */
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < W && y < H) {
+        float avg = 0.0f;
+        for (int range1 = -2; range1 < +2; ++range1) {
+            int yy = clamp(y + range1, 0, H - 1);
+            for (int range2 = -2; range2 <= +2; ++range2) {
+                int xx = clamp(x + range2, 0, W - 1);
+                avg += IMG_IN[yy * W + xx];
+            }
+        }
+        avg /= 25.0f;
+        IMG_OUT[y * W + x] = avg;
+    }
 }
 
 int main(void) // int argc, const char* argv[]
@@ -43,20 +61,24 @@ int main(void) // int argc, const char* argv[]
     }
 
     /* Allocate images on GPU */
-    TODO;
+    cudaMalloc(&IMG_IN, W * H);
+    cudaMalloc(&IMG_OUT, W * H);
 
     /* Copy img_in from main memory to GPU memory */
-    TODO;
+    cudaMemcpy(IMG_IN, img_in, W * H, cudaMemcpyHostToDevice);
 
     /* Launch kernel that computes result and writes it to IMG_OUT */
     dim3 threads_in_block(32, 32, 1); // Just an example; should work fine
-    TODO;
+    dim3 blocks_in_grid(W / threads_in_block.x + (W % threads_in_block.x != 0),
+                        H / threads_in_block.y + (H % threads_in_block.y != 0), 1);
+    img_proc<<<blocks_in_grid, threads_in_block>>>(IMG_IN, IMG_OUT, W, H);
 
     /* Copy result from GPU memory to main memory */
-    TODO;
+    cudaMemcpy(img_out, IMG_OUT, W * H, cudaMemcpyDeviceToHost);
 
     /* Release GPU arrays */
-    TODO;
+    cudaFree(IMG_IN);
+    cudaFree(IMG_OUT);
 
     /* Save image */
     {
@@ -70,7 +92,8 @@ int main(void) // int argc, const char* argv[]
     }
 
     /* Release arrays */
-    TODO;
+    free(img_in);
+    free(img_out);
 
     return 0;
 }
